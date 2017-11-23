d42b1eb874aaba5165f7affc3b89c77a2d6a7151
SAJJA Vinod <vinod.sajja@novatel.com>
2017-11-23 14:27:24 -0700

    wild

--- uia4/src/environments/environment.prod.ts
+++ uia4/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import * as env from './.env.json';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: 'https://www.mlbtwits.com/api/v1',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',

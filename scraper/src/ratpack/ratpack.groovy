b27b76d20871fdab53ffdd2535aa586fecd91ddf
SAJJA Vinod <vinod.sajja@novatel.com>
2017-11-24 15:12:04 -0700

    progress

--- scraper/src/ratpack/ratpack.groovy
+++ scraper/src/ratpack/ratpack.groovy
@@ -1,28 +1,27 @@
+import groovy.json.JsonSlurper
 import ratpack.groovy.template.MarkupTemplateModule
-
+import ratpack.http.client.HttpClient
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-  bindings {
-  }
+    handlers {
+        // http://localhost:5050/stats?mlbPlayerId=592450
+        get('stats') {
+            def mlbPlayerId = request.queryParams['mlbPlayerId']
+            def stats = []
 
-  handlers {
-    get {
-      render 'hello world'
+            def url = "http://m.mlb.com/player/$mlbPlayerId"
+            def doc = new XmlParser(new org.ccil.cowan.tagsoup.Parser()).parse(url)
+
+            def careerStats = doc.depthFirst().findAll {
+                it instanceof Node &&
+//                it.name() == 'div' &&
+                it.@class == 'player-stats-summary-large'
+            }
+
+            render new JsonSlurper().parseText(careerStats.toListString()).toString()
+        }
+        files { dir "public" }
     }
-
-    get('stats') {
-      def mlbPlayerId = request.queryParams['mlbPlayerId']
-
-      def stats = []
-
-      // avg runs hr rbi sb
-      // w ks era whip sv
-
-      render mlbPlayerId.toString()
-    }
-
-    files { dir "public" }
-  }
 }

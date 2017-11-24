f1ec0dc95236011ca59280b5f050c8537f1218f9
SAJJA Vinod <vinod.sajja@novatel.com>
2017-11-24 16:02:17 -0700

    hitting stats!

--- scraper/src/ratpack/ratpack.groovy
+++ scraper/src/ratpack/ratpack.groovy
@@ -1,5 +1,6 @@
+import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
-import ratpack.groovy.template.MarkupTemplateModule
+import org.ccil.cowan.tagsoup.Parser
 import ratpack.http.client.HttpClient
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
@@ -9,19 +10,29 @@ ratpack {
         // http://localhost:5050/stats?mlbPlayerId=592450
         get('stats') {
             def mlbPlayerId = request.queryParams['mlbPlayerId']
-            def stats = []
 
-            def url = "http://m.mlb.com/player/$mlbPlayerId"
-            def doc = new XmlParser(new org.ccil.cowan.tagsoup.Parser()).parse(url)
+//            def url = "http://m.mlb.com/player/$mlbPlayerId"
+//            def doc = new XmlParser(new org.ccil.cowan.tagsoup.Parser()).parse(url)
+//            def result = doc.'**'.find {
+//                it instanceof Node &&
+////                it.name() == 'div' &&
+//                        it.@id == 'careerPanel'
+//            }
 
-            def careerStats = doc.depthFirst().findAll {
-                it instanceof Node &&
-//                it.name() == 'div' &&
-                it.@class == 'player-stats-summary-large'
+            def mlbHittersAPI = "http://m.mlb.com/lookup/json/named.sport_hitting_composed.bam?game_type=%27R%27&league_list_id=%27mlb_hist%27&sort_by=%27season_asc%27&player_id=$mlbPlayerId"
+//            def pitchingStats = "http://m.mlb.com/lookup/json/named.sport_pitching_composed.bam?game_type=%27R%27&league_list_id=%27mlb_hist%27&sort_by=%27season_asc%27&player_id=$mlbPlayerId"
+
+            def result = new JsonSlurper().parseText(mlbHittersAPI.toURL().getText())
+
+            def hittingStats = result.sport_hitting_composed.sport_hitting_tm.queryResults.row
+            hittingStats.each { statLine ->
+                println "${statLine.season} ${statLine.team_abbrev } ${statLine.ab}ab ${statLine.r}runs ${statLine.hr}hr ${statLine.rbi}rbi ${statLine.sb}sb"
             }
-
-            render new JsonSlurper().parseText(careerStats.toListString()).toString()
+            render new JsonBuilder(hittingStats).toPrettyString()
         }
         files { dir "public" }
     }
 }
+
+
+

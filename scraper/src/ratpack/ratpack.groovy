cb8d60b6242f1943011c38216b76e07901289879
vsajja <vinod.sajja@gmail.com>
2017-12-06 13:19:37 -0700

    progress

--- scraper/src/ratpack/ratpack.groovy
+++ scraper/src/ratpack/ratpack.groovy
@@ -19,7 +19,10 @@ ratpack {
         all RequestLogger.ncsa(log)
 
         get('maps/places') {
-            def apiKey = 'AIzaSyBIVOsx4FeYY8ZrqvyToGS6Dx07nv1OIRw'
+            // vsajja
+//            def apiKey = 'AIzaSyBIVOsx4FeYY8ZrqvyToGS6Dx07nv1OIRw'
+            // pistamayne
+            def apiKey = 'AIzaSyAmftkf9z9JpMJjpBUjT0K8wGZU0hRdIYo'
 
             def outputFile = new File('C:\\Users\\vsajja\\Desktop\\google api\\results.csv')
             outputFile.delete()
@@ -27,43 +30,102 @@ ratpack {
 
             def columnStr = ['place_id',
                              'name',
-                             'icon',
-                             'lat',
-                             'lng',
-                             'rating',
-                             'types'
+                             'Formatted Address',
+                    'Address',
+                    'City',
+                    'State/Province',
+                    'ZIP/Postal Code',
+                    'Country',
+                    'Phone Number',
+//                    'Email Address',
+                    'Website',
+                    'Price Level',
+                    'Rating',
+                    'Types',
+                    'Latitude',
+                    'Longitude'
             ].join(',')
 
             outputFile.text += columnStr + '\n'
             println columnStr
 
-            def pageToken = ''
+//            def pageToken = ''
 
-            while(pageToken != null) {
-                def url = "https://maps.googleapis.com/maps/api/place/textsearch/json" +
-                        "?query=starbucks+seattle" +
-                        "&sensor=false" +
-                        "&key=$apiKey" +
-                        "&page_token=$pageToken"
+//            while(pageToken != null) {
 
-                def result = new JsonSlurper().parseText(url.toURL().getText())
-                def places = result.results
 
-                places.each { place ->
-                    outputFile.text += [place.get('place_id'),
-                                        place.get('name'),
-                                        place.get('icon'),
-                                        place.get('geometry').get('location').get('lat'),
-                                        place.get('geometry').get('location').get('lng'),
-                                        place.get('rating'),
-                                        place.get('types').toString(),
-                    ].join(',') + '\n'
+            def url = "https://maps.googleapis.com/maps/api/place/textsearch/json" +
+                    "?query=starbucks+seattle" +
+                    "&sensor=false" +
+                    "&key=$apiKey"
+//                    "&page_token=$pageToken"
+
+            def result = new JsonSlurper().parseText(url.toURL().getText())
+            def places = result.results
+
+            places.each { place ->
+
+                // get place details
+                def placeId = place.place_id
+
+                def detailsUrl = "https://maps.googleapis.com/maps/api/place/details/json" +
+                        "?placeid=$placeId" +
+                        "&key=$apiKey"
+
+                def detailsResult = new JsonSlurper().parseText(detailsUrl.toURL().getText())
+
+                def name = place.get('name')
+                def formattedAddress = detailsResult.result.formatted_address
+
+                def addressDetails = formattedAddress.split(',')
+
+                def address = null
+                def city = null
+                def stateOrProvince = null
+                def zipOrPostalCode = null
+                def country = null
+
+                if(addressDetails.size() == 5) {
+                    address = addressDetails[1]?.trim()
+                    city = addressDetails[2]?.trim()
+                    stateOrProvince = addressDetails[3]?.trim().split(' ')[0]?.trim()
+                    zipOrPostalCode = addressDetails[3]?.trim().split(' ')[1]?.trim()
+                    country = addressDetails[4]?.trim()
+                }
+                else {
+                    address = addressDetails[0]?.trim()
+                    city = addressDetails[1]?.trim()
+                    stateOrProvince = addressDetails[2]?.trim().split(' ')[0]?.trim()
+                    zipOrPostalCode = addressDetails[2]?.trim().split(' ')[1]?.trim()
+                    country = addressDetails[3]?.trim()
                 }
 
-                if(result?.get('next_page_token')) {
-                    pageToken = result?.get('next_page_token')
-                }
+                def phoneNumber = detailsResult.result.formatted_phone_number
+                def website = detailsResult.result.website
+                def priceLevel = detailsResult.result.price_level
+
+                outputFile.text += [placeId,
+                                    name,
+                                    formattedAddress,
+                                    address,
+                                    city,
+                                    stateOrProvince,
+                                    zipOrPostalCode,
+                                    country,
+                                    phoneNumber,
+                                    website,
+                                    priceLevel,
+                                    place.get('rating'),
+                                    place.get('types').toString(),
+                                    place.get('geometry').get('location').get('lat'),
+                                    place.get('geometry').get('location').get('lng')
+                ].join(',') + '\n'
             }
+
+            if(result?.get('next_page_token')) {
+                println "pageToken found: " + result?.get('next_page_token')
+            }
+//            }
             render outputFile.text
         }
 
